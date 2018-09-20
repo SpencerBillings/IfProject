489e913d16982a4b14e43f8505a1bc8ff3274439
SpencerBillings <spencerbillings2000@gmail.com>
2018-09-20 09:55:10 -0600

    Set the setters and got the getters

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -1,6 +1,29 @@
 package controller;
 
+//	   Imports
+import model.Pizza;
+import javax.swing.JOptionPane;
+
 public class IfController
 {
+//	   Data Members
+	private Pizza myPizza;
 
+//	   Constructors 
+	/**
+	 * (Used to build the instance of the IfController)
+	 */
+	public IfController()
+	{
+		myPizza = new Pizza();
+	}
+
+//	   Methods
+	/**
+	 * (Where the program calls the methods to run the program)
+	 */
+	public void start()
+	{
+	
+	}
 }
--- src/model/Pizza.java
+++ src/model/Pizza.java
@@ -2,5 +2,89 @@ package model;
 
 public class Pizza
 {
+//	   Data Members
+	private double pizzaSize;
+	private boolean hasCheese;
+	private int pepperoniCount;
+	private int greenPepperCount;
+	private boolean hasStuffedCrust;
 
+	/**
+	 * Default constructor for post customization
+	 * (Don't know the information)
+	 * Init data members with 'bad' values to be changed
+	 */
+	public Pizza()
+	{
+		this.pizzaSize = -99;
+		this.hasCheese = false;
+		this.pepperoniCount = -99;
+		this.greenPepperCount = -99;
+		this.hasStuffedCrust = false;
+	}
+	
+	/**
+	 * Default constructor for current customization
+	 * (Do know the information)
+	 * @param pizzaSize The size of the pizza
+	 */
+	public Pizza(double pizzaSize)
+	{
+		this.pizzaSize = pizzaSize;
+	}
+	
+	public void setPizzaSize(double size)
+	{
+		this.pizzaSize = size;
+	}
+	
+	public void setHasCheese(boolean cheese)
+	{
+		this.hasCheese = cheese;
+	}
+	
+	public void setPepperoniCount(int pepperoni)
+	{
+		this.pepperoniCount = pepperoni;
+	}
+	
+	public void setGreenPepperCount(int greenPeppers)
+	{
+		this.greenPepperCount = greenPeppers;
+	}
+	
+	public void setHasStuffedCrust(boolean stuffedCrust)
+	{
+		this.hasStuffedCrust = stuffedCrust;
+	}
+	
+	public double getPizzaSize()
+	{
+		return pizzaSize;
+	}
+	
+	public boolean getCheese()
+	{
+		return hasCheese;
+	}
+	
+	public int getPepperoni()
+	{
+		return pepperoniCount;
+	}
+	
+	public int getGreenPeppers()
+	{
+		return greenPepperCount;
+	}
+	
+	public boolean getStuffedCrust()
+	{
+		return hasStuffedCrust;
+	}
+	
+	public void updateToppings(int pepperoni, int greenPeppers)
+	{
+		
+	}
 }
