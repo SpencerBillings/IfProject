1129403dba1bbd261a90fca50e9059a0183f675f
SpencerBillings <spencerbillings2000@gmail.com>
2018-09-25 08:34:12 -0600

    Added various notes and a while loop in the controller class.

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -24,6 +24,30 @@ public class IfController
 	 */
 	public void start()
 	{
-	
+		loopy();
 	}
+	
+	/**
+	 * Define, test, and update
+	 */
+	private void loopy()
+	{
+		//  DEFINE the variable
+		boolean isDone = true;
+		int count = 0;
+		
+		while (isDone) // TEST the variable
+		{
+			JOptionPane.showMessageDialog(null, "Retry?");
+			// Eventually change the loop variable
+			count++;
+			if (count >= 5) // OBOE: Off By One Error
+			{
+				JOptionPane.showMessageDialog(null, "Could not reconnect.");
+				isDone = false; // UPDATE the variable
+			}
+		}
+	}
+	
+	
 }
